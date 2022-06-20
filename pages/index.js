e24a7c18f4072898a6f58d4479311de1288729c0
chelle tuerk <tuerkbot@talkingRobot.local>
2022-06-20 08:24:19 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import ProductListing from "@components/ProductListing";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { getProductList } from "@api/getProductList";
+
+export default function Home({ products }) {
+  return (
+    <>
+      <Head>
+        <title>LifeFitness</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main>
+        <ul className="product-grid">
+          {products.map((p, index) => {
+            return <ProductListing key={`product${index}`} product={p.node} />;
+          })}
+        </ul>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  let products = await fetch(
+    `${process.env.NETLIFY_URL}/.netlify/functions/get-product-list`
+  )
+    .then((res) => res.json())
+    .then((response) => {
+      return response.products.edges;
+    })
+
+  return {
+    props: {
+      products,
+    },
+  }
+}
