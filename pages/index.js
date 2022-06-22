aa63d27f3fa514122363bf07a1e3090595d179f8
chelle tuerk <tuerkbot@talkingRobot.local>
2022-06-22 13:43:48 -0700

    render search input

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import ProductListing from "@components/ProductListing";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import Search from "@components/Search";
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 import { getProductList } from "@api/getProductList";
 
 export default function Home({ products }) {
@@ -11,8 +13,8 @@ export default function Home({ products }) {
         <title>LifeFitness</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
+      <Search />
       <main>
         <ul className="product-grid">
           {products.map((p, index) => {
