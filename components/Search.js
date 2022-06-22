3f496a6385945d6f1f178ed7d9bf510280c1d0eb
chelle tuerk <tuerkbot@talkingRobot.local>
2022-06-22 14:10:38 -0700

    search functional - showing hits, but needs further work on filtering

--- components/Search.js
+++ components/Search.js
@@ -4,10 +4,10 @@ import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 export default function Search() {
   const searchClient = algoliasearch('J3WN417RXB', 'b9140c255c86b0f0ee283ab2c700ca57');
-  const indexName = algoliasearch('netlify_8291fd2f-32f9-4b82-aef4-9a702b45a98c_main_all');
+
   return (
-    <InstantSearch searchClient={searchClient} indexName={indexName}>
+    <InstantSearch searchClient={searchClient} indexName='netlify_8291fd2f-32f9-4b82-aef4-9a702b45a98c_main_all'>
       <SearchBox />
-      <Hits />
+        <Hits />
     </InstantSearch>
  )};
